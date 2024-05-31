c494b2a6dfaedfb4f2be3d8b19051328fe2308d7
miguelroblesuriel <miguel.roblesuriel@usp.ceu.es>
2024-05-31 20:25:38 +0200

    test machinelearning dataset

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from triplet_extraction.triplet_extraction import triplet_extraction
 
 
-input_filename = "230421-Exp-PAN-1-110-SMGA-MSMS1.mzMl"
+input_filename = "G75229_1x_10ul_RA7_01_19775.mzMl"
 triplets = triplet_extraction(input_filename)
 output_filename = input_filename.replace(".mzMl", "_triplets.npy")
 np.save(output_filename, triplets)
